a31c172f1db2eeb20ba494ad870803480a544eba
Andrew Bruce <andrew@camelpunch.com>
2014-12-10 13:10:18 +0000

    Move import next to usage
    Should help docs when they include a snippet from the settings file

--- pong_matcher_django/settings.py
+++ pong_matcher_django/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-from urllib.parse import urlparse
 
 
 # Quick-start development settings - unsuitable for production
@@ -59,6 +58,7 @@ WSGI_APPLICATION = 'pong_matcher_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+from urllib.parse import urlparse
 url = urlparse(
         os.environ.get(
             'DATABASE_URL',
